989df5721246f83215c5a7412c69826f4dc68d19
vinugant <vinugant@LIN76001841.corp.capgemini.com>
2021-02-19 14:48:35 +0530

    Payment controller and service

--- healthify/src/main/java/com/cg/healthify/controller/PaymentController.java
+++ healthify/src/main/java/com/cg/healthify/controller/PaymentController.java
@@ -16,37 +16,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.healthify.beans.NutritionPlan;
 import com.cg.healthify.beans.Payment;
 import com.cg.healthify.service.MapValidationErrorService;
 import com.cg.healthify.service.PaymentServiceImpl;
 
-
-
 @RestController
 @RequestMapping("/api/payment")
 public class PaymentController {
-@Autowired
-private PaymentServiceImpl paymentServiceImpl;
-@Autowired
-private MapValidationErrorService mapValidationErrorService;
+	@Autowired
+	private PaymentServiceImpl paymentServiceImpl;
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
 
-
-@PostMapping("/{paymentIdentifier}")
-public ResponseEntity<?> addAndUpdatePayment(@Valid @RequestBody Payment payment,BindingResult result,@PathVariable String paymentIdentifier){
-	ResponseEntity<?> errorMsg=mapValidationErrorService.mapValidationError(result);
-	if(errorMsg!=null) return errorMsg;	
-	Payment payment1=paymentServiceImpl.addPayment(paymentIdentifier, payment);
-	return new ResponseEntity<Payment>(payment1,HttpStatus.OK);
-}
+	@PostMapping("/{paymentIdentifier}")
+	public ResponseEntity<?> addAndUpdatePayment(@Valid @RequestBody Payment payment, BindingResult result,
+			@PathVariable String paymentIdentifier) {
+		ResponseEntity<?> errorMsg = mapValidationErrorService.mapValidationError(result);
+		if (errorMsg != null)
+			return errorMsg;
+		Payment payment1 = paymentServiceImpl.addPayment(paymentIdentifier, payment);
+		return new ResponseEntity<Payment>(payment1, HttpStatus.OK);
+	}
 
 	@GetMapping("/{transactionId}")
-	public Payment getPaymentDetails(@PathVariable String transactionId){ 
+	public Payment getPaymentDetails(@PathVariable String transactionId) {
 		return paymentServiceImpl.findPaymentByTransactionId(transactionId);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> DeletePaymentById(@PathVariable Long id){
-		 paymentServiceImpl.deletePaymentByTransactionId(id);
-		return new ResponseEntity<String>("Payment having Transaction ID: "+id+" is deleted.",HttpStatus.OK);
+	public ResponseEntity<?> DeletePaymentById(@PathVariable Long id) {
+		paymentServiceImpl.deletePaymentByTransactionId(id);
+		return new ResponseEntity<String>("Payment having Transaction ID: " + id + " is deleted.", HttpStatus.OK);
 	}
+
+	@GetMapping("/allpayments")
+	public List<Payment> getAllPayments() {
+		return paymentServiceImpl.getAllPayments();
 	}
+}
--- healthify/src/main/java/com/cg/healthify/service/PaymentService.java
+++ healthify/src/main/java/com/cg/healthify/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.cg.healthify.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.cg.healthify.beans.Payment;
@@ -7,7 +9,11 @@ import com.cg.healthify.beans.Payment;
 @Service
 public interface PaymentService {
 	public Payment addPayment(String paymentIdentifier, Payment payment);
+
 	public Payment findPaymentByTransactionId(String transactionId);
+
 	public void deletePaymentByTransactionId(Long id);
+
+	public List<Payment> getAllPayments();
 
 }
